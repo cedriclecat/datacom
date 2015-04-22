843915f3e58f1c4aaf6dfb4a4dac0df14fe9af7e
cedriclecat <cedric.lecat@student.howest.be>
2015-04-22 17:13:12 +0200

    fixes
    slower move when zoomed in

--- IPCAM/IPCAM/Form1.cs
+++ IPCAM/IPCAM/Form1.cs
@@ -13,7 +13,7 @@ namespace IPCAM
 {
     public partial class Form1 : Form
     {
-        
+        int panzoom = 0;
         int pan = -42;
         int tilt = -16;
       
@@ -61,23 +61,32 @@ namespace IPCAM
         private void BtnDown_Click(object sender, EventArgs e)
         {
             tilt -= 5;
+            if(tilt <  -90)
+            {
+                if (pan >= 0) pan -= 180;
+                else if (pan < 0) pan += 180;
+            }
             WebRequest request = WebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&tilt=" + tilt);
             requesthandling(request);
         }
 
         private void BtnRight_Click(object sender, EventArgs e)
         {
-            pan += 20;
+            pan += 20-panzoom;
+            
             if (pan >= 180) pan -= 360;
-            WebRequest request = WebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&pan="+pan);
+
+            WebRequest request = WebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&pan=" + pan);
             requesthandling(request);
           
         }
 
         private void BtnLeft_Click(object sender, EventArgs e)
         {
-            pan -= 20;
+            
+            pan -= 20 + panzoom;              
             if (pan <= -180) pan += 360;
+
             WebRequest request = WebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&pan=" + pan);
             requesthandling(request);
         }
@@ -90,15 +99,17 @@ namespace IPCAM
 
         private void BtnZoomIn_Click(object sender, EventArgs e)
         {
-            int zoom = 100;           
+            int zoom = 200;
+            if (panzoom < 19) panzoom++;
+            
             WebRequest request = WebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&rzoom=" + zoom);
             requesthandling(request);
         }
 
         private void BtnZoomOut_Click(object sender, EventArgs e)
         {
-
-            int zoom = -100;
+            if (panzoom > 0) panzoom--;
+            int zoom = -200;
             WebRequest request = WebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&rzoom=" + zoom);
             requesthandling(request);
 
@@ -117,5 +128,7 @@ namespace IPCAM
             WebRequest request = WebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&rfocus=" + focus);
             requesthandling(request);
         }
+
+        //http://172.23.49.1//axis-cgi/com/ptz.cgi?continuouspantiltmove=-0,-0
     }
 }
